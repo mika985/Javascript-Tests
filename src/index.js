74b2914d81500da966359443cc9f3b685d9d9c72
mickla515 <michelangelo.giorgini@gmail.com>
2025-12-03 10:39:02 +0100

    Implement countdown timer functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('Hello from javascript-tests scaffold');
+$(document).ready(function () {
+  let timer = 5;
+
+  // Assegna setInterval a una variabile per poterlo fermare
+  let countdown = setInterval(function () {
+    timer = timer - 1;
+    $(".score").text(timer);
+
+    // Se vuoi che si fermi:
+    if (timer <= 0) {
+      clearInterval(countdown);
+      $(".score").text("Tempo scaduto!");
+    }
+  }, 1000);
+});
